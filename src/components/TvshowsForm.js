4504290659374321cda34cd3541c192acd01f687
alexlecco <alexlecco24@gmail.com>
2020-05-19 23:22:57 -0300

    implement POST requests to db

---
+++ src/components/TvshowsForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class TvshowsForm extends Component {
+    render() {
+        return(
+            <div className="component-container">
+                <h3>TvshowsForm</h3>
+
+                
+            </div>
+        )
+    }
+}
