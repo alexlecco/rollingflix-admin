dfdaf68b0e6d0f5cf084d74ad0463431ac2dfe11
alexlecco <alexlecco24@gmail.com>
2020-05-20 22:23:32 -0300

    implement DELETE request for tvshows

--- src/components/TvshowsTable.js
+++ src/components/TvshowsTable.js
@@ -1,48 +1,75 @@
 import React, { Component } from 'react';
-import { Table } from 'antd';
+import { Table, Popconfirm } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-
-const columns = [
-  { title: 'Nombre', dataIndex: 'title', key: 'title' },
-  { title: 'Año', dataIndex: 'year', key: 'year' },
-  { title: 'Pais', dataIndex: 'country', key: 'country' },
-  { title: 'Poster', dataIndex: 'poster', key: 'poster' },
-  { title: 'Temporadas', dataIndex: 'seasons', key: 'seasons' },
-  { title: 'Categoria', dataIndex: 'genre', key: 'genre' },
-  { title: 'Resumen', dataIndex: 'summary', key: 'summary' },
-  {
-    dataIndex: '',
-    fixed: 'right',
-    key: 'x',
-    width: 40,
-    render: () => <EditOutlined twoToneColor="#52c41a" />,
-  },
-  {
-    dataIndex: '',
-    fixed: 'right',
-    key: 'x',
-    width: 40,
-    render: () => <DeleteOutlined twoToneColor="#eb2f96" />,
-  }
-];
 
 export default class TvshowsTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      columns: []
     }
   }
 
   componentDidMount() {
-    this.fetchData()
+    this.fetchData();
+    this.setColumns()
   }
 
   fetchData() {
+    console.log("por fetchear")
     fetch('http://localhost:3001/tvshows')
-      .then(res => res.json())
+      .then(response => response.json())
       .then(data => this.setState({ data }))
+  }
+
+  setColumns() {
+    const columns = [
+      { title: 'Nombre', dataIndex: 'title', key: 'title' },
+      { title: 'Año', dataIndex: 'year', key: 'year' },
+      { title: 'Pais', dataIndex: 'country', key: 'country' },
+      { title: 'Poster', dataIndex: 'poster', key: 'poster' },
+      { title: 'Temporadas', dataIndex: 'seasons', key: 'seasons' },
+      { title: 'Categoria', dataIndex: 'genre', key: 'genre' },
+      { title: 'Resumen', dataIndex: 'summary', key: 'summary' },
+      {
+        dataIndex: '',
+        fixed: 'right',
+        key: 'x',
+        width: 40,
+        render: () => <EditOutlined twoToneColor="#52c41a" />,
+      },
+      {
+        dataIndex: '',
+        fixed: 'right',
+        key: 'x',
+        width: 40,
+        render: (tvshow) =>
+          this.state.data.length >= 1 ? (
+            <Popconfirm title="¿Quieres eliminar?" onConfirm={() => this.handleDelete(tvshow._id)}>
+              <DeleteOutlined twoToneColor="#eb2f96" />
+            </Popconfirm>
+          ) : null,
+      }
+    ];
+
+    this.setState({ columns })
+  }
   
+  handleDelete(tvshowId) {
+    const data = [...this.state.data];
+    this.setState({
+      data: data.filter(tvshow => tvshow._id !== tvshowId),
+    });
+  
+    const requestOptions = {
+      method: 'DELETE'
+    }
+  
+    console.log("por borrar")
+    fetch("http://localhost:3001/tvshows/" + tvshowId, requestOptions)
+      .then(res => res.json())
+      .then(() => { console.log("tvshow deleted") })
   }
 
   render() {
@@ -50,7 +77,7 @@ export default class TvshowsTable extends Component {
       <div className="component-container">
         <h3>TvshowsTable</h3>
         <Table
-          columns={columns}
+          columns={this.state.columns}
           dataSource={this.state.data}
           scroll={{ x: 1500, y: 300 }}
         />
